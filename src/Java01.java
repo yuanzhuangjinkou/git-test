7fcab30932a7827d2b91d8bbb9549df4d8efed43
yuanzhuangjinkou <101234254+yuanzhuangjinkou@users.noreply.github.com>
2023-05-07 13:16:08 +0800

    Update Java01.java
    提交2

--- src/Java01.java
+++ src/Java01.java
@@ -3,6 +3,7 @@ public class Java01 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("231");
+        System.out.println("2134123");
     }
 
 }
